05802147057021877f7b8ca00f2345a42e256ab6
Diego Matienzo <diegomc365@hotmail.com>
2020-09-08 22:39:13 -0500

    add container props

---
+++ pages/_app.js
@@ -0,0 +1,45 @@
+import App from 'next/app'
+import Head from 'next/head'
+import NavBar from '../components/navbar'
+import Footer from '../components/footer'
+
+class MovieApp extends App {
+
+    // TODO: exectute here getInitialProps and pass this data to your page
+
+    static async getInitialProps(appContext) {
+        // Executing getInitialProps of page you are navigated to
+        const appProps = await App.getInitialProps(appContext)
+        return { appProps }
+    }
+
+    render() {
+        // component holds page you are navigating to
+        const { Component, appProps } = this.props
+
+        return (
+            <div>
+                <Head>
+                    <title>Home</title>
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
+                    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
+                    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
+                </Head>
+                <NavBar />
+                <div className="base-page">
+                    <Component {...appProps.pageProps}/>
+                </div>
+                <Footer />
+                <style jsx>{`
+                    .base-page {
+                        padding-top: 80px;
+                    }
+                `}
+                </style>
+            </div>
+        )
+    }
+}
+
+export default MovieApp
